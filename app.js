d3daf8e21f2bed03c851f0e717b00fb0a36c4cb1
Harande <selchanminer@gmail.com>
2023-03-09 14:45:36 +0000

    💡 Added Models and set authorization

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 const express = require('express')
 const authRoutes = require('./routes/auth')
 const passportRun = require('./passport')
-require('./config/database')
+const passport = require('passport')
+const session = require('express-session')
+const parser = require('body-parser')
+const request = require('request')
+//require('./config/database')
+require('dotenv').config()
 
 const app = express()
+
+app.use(session({
+    secret: process.env.secret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { 
+        maxAge:1000*60*60*24
+     }
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 //import auth routes from auth.js
 app.use('/auth',authRoutes)
 
+app.use(parser.json())
 
-app.get('/',(req,res,next)=>{
-    res.send("<h1>Login</h1>")
+app.get('/',(req,res)=>{
+   res.send('dfdfd')
 })
+
 
 
 
