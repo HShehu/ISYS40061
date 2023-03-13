bec919833caceb4dfee27af3a037ab825cad4bea
Harande <selchanminer@gmail.com>
2023-03-13 10:26:44 +0000

    Dialog modal added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const passportRun = require('./passport')
 const passport = require('passport')
 const session = require('express-session')
 const parser = require('body-parser')
+const User = require('./models/user-model')
 require('dotenv').config()
 require('./config/database')
 
@@ -36,6 +37,10 @@ const authCheck = (req,res,next) =>{
     }
 }
 
+const addToWallet = (user,amount)=>{
+    User.findOneAndUpdate({googleId:user.googleId},{walletUSD:user.walletUSD+amount})
+}
+
 app.get('/',authCheck,(req,res)=>{
 
      fetch('http://prpoject-api-1:8000/listStock')
@@ -43,8 +48,32 @@ app.get('/',authCheck,(req,res)=>{
      .then((data)=>res.render('home',{stocks:data.stocks,user:req.user}))
      .catch((error) => {
         console.error("Error:", error)
-      }) 
-      console.log(req.user.username)
+      })
+})
+
+app.get('/addMoney',(req,res)=>{
+    var number = prompt("Add Amount to Wallet", "Type your number here");
+    number = parseInt(number);
+    if (isNaN(number))
+   {
+   alert("Numbers only");
+   }
+    else
+   {
+   if (number == 0)
+      {
+      alert("The number is zero");
+      }
+   else if (number<0)
+      {
+      alert("Must be positive");
+      }
+   else
+      {
+      alert(`${number} is added to wallet`);
+      addToWallet(req.user,number)
+      }
+   }
 })
 
 app.listen(3000)
